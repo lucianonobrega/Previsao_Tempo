386b5e4275100ad2016e18170d0a6b4e2cb27fd0
Luciano Nobrega <luciano.lp@hotmail.com>
2024-04-29 17:52:48 -0300

    previsao_tempo v1.2

--- previsao_tempo.py
+++ previsao_tempo.py
@@ -1,9 +1,8 @@
 import requests
-import api_dados
 
-key = api_dados.Key
-lat = api_dados.Lat
-lon = api_dados.Lon
+key = "chave_da_api"
+lat = "lat"
+lon = "lon"
 
 try:
       response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}&units=metric&lang=pt_br")
@@ -17,6 +16,9 @@ try:
       umidade = dados['main']['humidity']
       pais = dados['sys']['country']
       cidade = dados['name']
+
+      print(f"Status Code: {response.status_code}\n"
+            f"Reason: {response.reason}\n")
 
       print(f"País:{pais}.\n"
             f"Cidade:{cidade}.\n"
